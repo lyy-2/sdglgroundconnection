2aa14c1436996cb14de6128bddcd5f91dc1c9528
lyy-2 <40796526+lyy-2@users.noreply.github.com>
2018-08-19 21:53:07 +0800

    Update StaffServiceImpl.java

--- src/main/java/com/dyhc/sdglgroundconnection/service/impl/StaffServiceImpl.java
+++ src/main/java/com/dyhc/sdglgroundconnection/service/impl/StaffServiceImpl.java
@@ -14,4 +14,60 @@ public class StaffServiceImpl implements StaffService {
 
     @Autowired
     private StaffMapper staffMapper;
+    
+    /**
+     * 分页查看全部
+     * @param pageNo
+     * @param PageSize
+     * @param staffname
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public PageInfo<Staff> listStaffLike(Integer pageNo, Integer PageSize, String staffname) throws Exception {
+        PageHelper.startPage(pageNo,PageSize,true);
+        PageInfo<Staff> staff=new PageInfo<>(staffMapper.getStaffAll(staffname));
+        return staff;
+    }
+
+    /**
+     * 删除  修改
+     * @param staff
+     * @return
+     */
+    @Override
+    public Integer getStaffUpd(Staff staff) {
+        return staffMapper.getStaffUpd(staff);
+    }
+
+    /**
+     * 修改
+     * @param staff
+     * @return
+     */
+    @Override
+    public Integer getStaffUpdTwo(Staff staff) {
+        return staffMapper.getStaffUpdTwo(staff);
+    }
+
+    /**
+     * 添加
+     * @param staff
+     * @return
+     */
+    @Override
+    public Integer getStaffAdd(Staff staff) {
+        return staffMapper.insert(staff);
+    }
+
+    /**
+     * 修改  查看
+     * @param staffId
+     * @return
+     */
+    @Override
+    public Staff getStaffUpdTwoById(Integer staffId) {
+        return staffMapper.getStaffUpdTwoById(staffId);
+    }
+    
 }
