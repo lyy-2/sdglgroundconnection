e8ea4e5758cb3411b1776c9602f85d019dcffab9
lyy-2 <40796526+lyy-2@users.noreply.github.com>
2018-08-19 21:09:40 +0800

    Update TravelService.java

--- src/main/java/com/dyhc/sdglgroundconnection/service/TravelService.java
+++ src/main/java/com/dyhc/sdglgroundconnection/service/TravelService.java
@@ -9,4 +9,57 @@ import org.springframework.stereotype.Service;
  * 组团社业务接口
  **/
 public interface TravelService {
+  /**
+     * 添加信息
+     * @param travel
+     * @return
+     */
+    Integer getAddTravel(Travel travel) throws Exception;
+    /**
+     * 修改信息
+     * @param travel
+     * @return
+     */
+    Integer getTravelUpd(Travel travel) throws Exception;
+
+    /**
+     * a按条件模糊查看
+     * @param pageNo
+     * @param PageSize
+     * @return
+     * @throws Exception
+     */
+    PageInfo<Travel> listTravelLike(Integer pageNo, Integer PageSize,String travelName) throws Exception;
+
+    /**
+     * 按条件查看
+     * @param travelId
+     * @return
+     * @throws Exception
+     */
+    Integer getselectById(Integer travelId) throws Exception;
+
+
+    /**
+     * 删除    修改
+     * @param travelId
+     * @return
+     */
+    Integer getUpdTrave(Integer travelId);
+
+    /**
+     * 修改查看
+     * @param travelId
+     * @return
+     */
+    Travel getupdBytravelId(Integer travelId);
+
+
+    /**
+     * 修改 第二次
+     * @return
+     */
+    Integer getupdlala(Travel travel);
+  
+  
 }
