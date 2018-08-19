60284b76a61f72492db9fced98dc11a4c5c7f473
lyy-2 <40796526+lyy-2@users.noreply.github.com>
2018-08-19 21:51:39 +0800

    Update StaffService.java

--- src/main/java/com/dyhc/sdglgroundconnection/service/StaffService.java
+++ src/main/java/com/dyhc/sdglgroundconnection/service/StaffService.java
@@ -8,4 +8,41 @@ import org.springframework.stereotype.Component;
  * 人员业务接口
  **/
 public interface StaffService {
+  
+  /**
+     * 分页查看全部
+     * @param pageNo
+     * @param PageSize
+     * @param staffname
+     * @return
+     * @throws Exception
+     */
+    PageInfo<Staff> listStaffLike(Integer pageNo, Integer PageSize,String staffname) throws Exception;
+    /**
+     * 删除 修改
+     * @param staff
+     * @return
+     */
+    Integer getStaffUpd(Staff staff);
+
+    /**
+     * 修改
+     * @param staff
+     * @return
+     */
+    Integer getStaffUpdTwo(Staff staff);
+
+    /**
+     * 添加
+     * @param staff
+     * @return
+     */
+    Integer getStaffAdd(Staff staff);
+
+    /**
+     * 修改  查看
+     * @param staffId
+     * @return
+     */
+    Staff getStaffUpdTwoById(Integer staffId);
 }
