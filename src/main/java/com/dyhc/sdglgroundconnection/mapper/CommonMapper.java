7b8247b05dee0667a03efe9bedc83745b9f8875c
lyy-2 <40796526+lyy-2@users.noreply.github.com>
2018-08-19 21:38:58 +0800

    Update CommonMapper.java

--- src/main/java/com/dyhc/sdglgroundconnection/mapper/CommonMapper.java
+++ src/main/java/com/dyhc/sdglgroundconnection/mapper/CommonMapper.java
@@ -8,4 +8,18 @@ import tk.mybatis.mapper.common.MySqlMapper;
  * 公共的mapper接口，自己的mapper接口要继承该接口，可以使用mybatis的公共插件
  **/
 public interface CommonMapper<T> extends Mapper<T>, MySqlMapper<T> {
+  
+  /**
+     * 查看全部
+     * @return
+     */
+    Company getCompany();
+
+    /**
+     * 修改信息
+     * @param company
+     * @return
+     */
+    Integer getUpdCompany(Company company);
+  
 }
